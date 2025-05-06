f1b9cb8c869a31b5ebdd127ff020451631c4f553
rohandhadke <rohandhadke7620@gmail.com>
2025-05-07 01:05:02 +0530

    update

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,18 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
     content: [
-      "./index.html",
-      "./src/**/*.{js,jsx}",
+      "./src/**/*.{js,jsx,ts,tsx}",
     ],
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+          indigo: {
+            900: '#1a1a4a',
+          },
+        },
+        transitionDuration: {
+          'slow': '500ms',
+        }
+      },
     },
     plugins: [],
   }
-  
